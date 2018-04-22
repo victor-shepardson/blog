469df9a1c3836f0b346ca10ea6480a3bad6dbc43
Victor Shepardson <victor.shepardson@gmail.com>
2018-04-22 18:54:32 -0400

    Nikola auto commit.
    Nikola version: 7.8.14

--- conf.py
+++ conf.py
@@ -18,7 +18,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Victor Shepardson"  # (translatable)
-BLOG_TITLE = "Project Blog"  # (translatable)
+BLOG_TITLE = "~/blog"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://victor-shepardson.github.io/blog/"
